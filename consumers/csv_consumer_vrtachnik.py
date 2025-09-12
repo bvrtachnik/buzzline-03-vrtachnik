9bc5179710dd4d8ea53028d57bde14af1030245b
Brett Vrtachnik <b.vrtachnik@gmail.com>
2025-09-11 21:27:50 -0400

    Run csv producer and consumer

--- consumers/csv_consumer_vrtachnik.py
+++ consumers/csv_consumer_vrtachnik.py
@@ -1,11 +1,10 @@
 """
 csv_consumer_vrtachnik.py
 
-Consume json messages from a Kafka topic and process them.
+Consume custom messages from a Kafka topic and process them.
 
 Example Kafka message format:
-{"timestamp": "2025-01-11T18:15:00Z", "temperature": 225.0}
-
+{"message": "Data engineering is confusing sometimes.", "author": "Eve"}
 """
 
 #####################################
